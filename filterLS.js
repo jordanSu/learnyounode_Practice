774775be2e94d587dec31170240a9dfbf5fd4531
jorrdansu1126 <newjordansu1126@gmail.com>
2015-09-10 16:52:21 +0800

    filterLS pass

--- filterLS.js
+++ filterLS.js
@@ -1,6 +1,13 @@
 var fs = require('fs');
+var path = require('path');
+var ext = '.' + process.argv[3];
 fs.readdir(process.argv[2],callback);
 
 function callback(err,list){
-  
+    if (!err) {
+        for (var file in list) {
+            if (path.extname(list[file]) == ext)
+                console.log(list[file]);
+        }
+    }
 }
